680e2f8644640cdbb020a8b844617ee495086421
Tieme Woudstra <tiemewoudstra@hotmail.com>
2017-05-13 17:35:10 +0200

    Voor maincharacters boolean frontposition toegevoegd en in battleview wordt indien true het maincharacter meer naar links geplaatst.
    Voor enemies int x_position en y_position aangemaakt. In battleview wordt enemy geplaatst in het scherm o.b.v. y_position.
    
    Voor x_position moet nog worden aangemaakt. Enemies moeten kunnen verschijn o.b.v. 10 x 5 posities (in totaal dus 50 verschillende posities). Hiermee is ook het maximum aan enemies tegelijkertijd in een gevecht bepaald.

--- src/main/java/nl/woudstra/sprookjesbos/GUI/Batle/Battleview.java
+++ src/main/java/nl/woudstra/sprookjesbos/GUI/Batle/Battleview.java
@@ -1,6 +1,7 @@
 package nl.woudstra.sprookjesbos.GUI.Batle;
 
 import nl.woudstra.sprookjesbos.characters.MainCharacter;
+import nl.woudstra.sprookjesbos.characters.enemies.AngryBird;
 import nl.woudstra.sprookjesbos.characters.enemies.Enemy;
 import nl.woudstra.sprookjesbos.characters.enemies.TestEnemy;
 
@@ -16,21 +17,34 @@ import java.io.IOException;
 public class Battleview extends JPanel {
     private MainCharacter[] mainCharacters;
     private Enemy[] enemies;
+
     private Image[] scaledMainCharacterImages;
+    private Image[] scaledEnemyImages;
     private Image scaledBackGroundImage;
+
+    private double[] y1;     // for x,y position maincharacters
+    private double[] x1;
 
 
     private BufferedImage backgroundBattle;
     private BufferedImage knightImage;
 
     public Battleview(MainCharacter[] mainCharacters) {
-        enemies = new Enemy[]{new TestEnemy()};         //tijdelijk om te testen
+        enemies = new Enemy[]{new AngryBird(1,1), new AngryBird(3,3)};         //temporary for testing
+
+        y1 = new double[]{0.25, 0.4, 0.55, 0.7, 0.85};                         //x, y positions maincharacters
+        x1 = new double[]{0.83, 0.9};
 
         this.mainCharacters = mainCharacters;
         try {
             backgroundBattle = ImageIO.read(getClass().getResource("/images/Weiland.JPG"));
-            for(int i = 0; i < mainCharacters.length;i++){
+
+            for(int i = 0; i < mainCharacters.length;i++){                    //load images maincharacters
                 mainCharacters[i].setCharacterImage(ImageIO.read(getClass().getResource(mainCharacters[i].getImageLocation())));
+            }
+
+            for(int i = 0;i<enemies.length;i++){
+                enemies[i].setCharacterImage(ImageIO.read(getClass().getResource(enemies[i].getImageLocation())));
             }
 
         } catch (IOException ex) {
@@ -45,17 +59,28 @@ public class Battleview extends JPanel {
         int x = super.getWidth();
         int y = super.getHeight();
 
-        int x1 = (int) (0.8 * x);
-        int y1 = (int) (0.3 * y);
+        scaledBackGroundImage = backgroundBattle.getScaledInstance(x,y,Image.SCALE_SMOOTH);   //images scaled on screenwidth/height
 
-        scaledBackGroundImage = backgroundBattle.getScaledInstance(x,y,Image.SCALE_SMOOTH);
+        scaledMainCharacterImages = new Image[mainCharacters.length];
+        for(int i = 0; i < mainCharacters.length;i++){
+            scaledMainCharacterImages[i] = mainCharacters[i].getCharacterImage().getScaledInstance(x / 13,y / 10,Image.SCALE_SMOOTH );
+        }
 
-        Image scaledKnight = mainCharacters[0].getCharacterImage().getScaledInstance(x / 13,y / 10,Image.SCALE_SMOOTH );
+        scaledEnemyImages = new Image[enemies.length];
+        for(int i = 0;i<enemies.length;i++){
+            scaledEnemyImages[i] = enemies[i].getCharacterImage().getScaledInstance(x / 13,y / 10,Image.SCALE_SMOOTH );
+        }
 
-        g.drawImage(scaledBackGroundImage, 0, 0, this); // see javadoc for more info on the parameters
-
-        g.drawImage(scaledKnight,x1, y1,this);
-
-
+        g.drawImage(scaledBackGroundImage, 0, 0, this);                        // draw scaled background, maincharacters, enemies
+        for(int i = 0; i < mainCharacters.length;i++){
+            if(mainCharacters[i].isFrontPosition()){
+                g.drawImage(scaledMainCharacterImages[i],((int) (x1[0] * x)),((int) (y1[i] * y)),this);
+            } else{
+                g.drawImage(scaledMainCharacterImages[i],((int) (x1[1] * x)),((int) (y1[i] * y)),this);
+            }
+        }
+        for(int i = 0; i<enemies.length;i++){
+            g.drawImage(scaledEnemyImages[i],0,((int) (y1[enemies[i].getY_position()-1] * y)),this);
+        }
     }
 }
